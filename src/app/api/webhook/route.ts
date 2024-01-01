87b0c431726b034a29e2a471123eb97800eeec33
JENNIFER DE LEO <deleo.jennifer@yahoo.fr>
2024-01-01 18:47:06 +0100

    final build

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,83 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+import { createBooking, updateHotelRoom } from "@/libs/apis";
+import { Metadata } from "@stripe/stripe-js";
+
+const checkout_session_completed = "checkout.session.completed";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2023-10-16",
+});
+
+interface SessionMetadata {
+  adults: string;
+  checkinDate: string;
+  checkoutDate: string;
+  children: string;
+  hotelRoom: string;
+  numberOfDays: string;
+  user: string;
+  discount: string;
+  totalPrice: string;
+}
+
+export async function POST(req: Request, res: Response) {
+  const reqBody = await req.text();
+  const sig = req.headers.get("stripe-signature");
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  let event: Stripe.Event;
+
+  try {
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 500 });
+  }
+
+  // load our event
+  switch (event.type) {
+    case checkout_session_completed:
+      const session = event.data.object;
+
+      const {
+        adults,
+        checkinDate,
+        checkoutDate,
+        children,
+        hotelRoom,
+        numberOfDays,
+        user,
+        discount,
+        totalPrice,
+      } = session.metadata as Metadata;
+
+      await createBooking({
+        adults: Number(adults),
+        checkinDate,
+        checkoutDate,
+        children: Number(children),
+        hotelRoom,
+        numberOfDays: Number(numberOfDays),
+        discount: Number(discount),
+        totalPrice: Number(totalPrice),
+        user,
+      });
+
+      await updateHotelRoom(hotelRoom);
+
+      return NextResponse.json("Booking successful", {
+        status: 200,
+        statusText: "Booking Successful",
+      });
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return NextResponse.json("Event Received", {
+    status: 200,
+    statusText: "Event Received",
+  });
+}
